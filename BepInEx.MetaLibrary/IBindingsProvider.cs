dda7a7a0f7926bac0c9e21a178c21e5ec9d9b819
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-13 19:40:32 +0100

    replace `SigurdBusSupplier` with a documented `SearchForBus` method

--- BepInEx.MetaLibrary/IBindingsProvider.cs
+++ BepInEx.MetaLibrary/IBindingsProvider.cs
@@ -1,9 +1,13 @@
-using System;
 using Bus.Api;
 
 namespace MetaLibrary;
 
 public interface IBindingsProvider
 {
-    public Func<IEventBus> SigurdBusSupplier { get; }
+    /// <summary>
+    /// Event bus search function. Search functions are expected to take one argument, being the bus name - usually the
+    /// BepInEx plugin GUID of a MetaPlugin - and return an <see cref="IEventBus"/> instance.
+    /// In case a search function cannot find a given event bus, it should return <see langword="null"/>.
+    /// </summary>
+    public IEventBus? SearchForBus(string busIdentifier) => null;
 }
