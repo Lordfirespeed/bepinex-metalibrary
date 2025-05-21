534d40181bc94cb755194f775e19e45aaf30cae0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-21 20:13:49 +0100

    make `MetaLibrary` static

--- BepInEx.MetaLibrary/MetaLibrary.cs
+++ BepInEx.MetaLibrary/MetaLibrary.cs
@@ -2,7 +2,7 @@ using Bus.Api;
 
 namespace MetaLibrary;
 
-public class MetaLibrary
+public static class MetaLibrary
 {
     public static readonly IEventBus EventBus = new BusConfiguration {
         StartImmediately = false,
