024ae65df2d4cbeb16db4345462f4ddec01e5f84
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-21 20:19:14 +0100

    add `AcceptEvent` method to plugin container

--- BepInEx.MetaLibrary/PluginContainer.cs
+++ BepInEx.MetaLibrary/PluginContainer.cs
@@ -3,9 +3,11 @@
  * The Sigurd Team licenses this file to you under the LGPL-3.0-OR-LATER license.
  */
 
+using System;
 using System.Reflection;
 using BepInEx;
 using Bus.Api;
+using MetaLibrary.Event;
 
 namespace MetaLibrary;
 
@@ -25,6 +27,32 @@ public class PluginContainer
     {
         Info = info;
         Assembly = assembly;
+    }
+
+    public void AcceptEvent<T>(T @event) where T : Bus.Api.Event, IPluginBusEvent
+    {
+        try {
+            // logger.trace: firing event ...
+            EventBus.Post(@event);
+            // logger.trace: fired event ...
+        }
+        catch (Exception exc) {
+            // logger.error: caught exception ...
+            throw new InvalidOperationException($"An error occurred during event {typeof(T).Name} affecting plugin {Info}", exc);
+        }
+    }
+
+    public void AcceptEvent<T>(EventPriority phase, T @event) where T : Bus.Api.Event, IPluginBusEvent
+    {
+        try {
+            // logger.trace: firing event ...
+            EventBus.Post(phase, @event);
+            // logger.trace: fired event ...
+        }
+        catch (Exception exc) {
+            // logger.error: caught exception ...
+            throw new InvalidOperationException($"An error occurred during event {typeof(T).Name} affecting plugin {Info}", exc);
+        }
     }
 
     /// <inheritdoc />
